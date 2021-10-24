04ef946fc7ef92b474745dcf48577a6712a32383
alexhersfeldt <39616651+alexhersfeldt@users.noreply.github.com>
2021-10-24 21:15:04 +0200

    Initial Commit

---
+++ src/main/java/alexhersfeldt/iver_studio/IverStudioApplication.java
@@ -0,0 +1,13 @@
+package alexhersfeldt.iver_studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IverStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IverStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alexhersfeldt/iver_studio/IverStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package alexhersfeldt.iver_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IverStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
